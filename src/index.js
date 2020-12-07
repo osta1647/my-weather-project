b9d053f612845f9a942f85c0883154e05fa92e26
osta1647 <72413097+osta1647@users.noreply.github.com>
2020-12-07 16:26:18 -0600

    Addded wind and revised city replacing function

--- src/index.js
+++ src/index.js
@@ -1,45 +1,52 @@
 function formatDate(timestamp) {
-  let date= new Date(timestamp);
-    let hours = date.getHours();
-    if (hours < 10) {
-      hours = `0${hours}`;
-    }
-    let minutes = date.getMinutes();
-    if (minutes < 10) {
-      minutes = `0${minutes}`;
-    }
-    let dayIndex = date.getDay();
-    let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
-    let day = days[dayIndex];
-    return `${day} ${hours}:${minutes}`;
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
   }
-  function showTemperature(response) {
-    console.log(response.data.main.temp);
-    let humidityElement= document.querySelector ("#humidity")
-    let descriptionElement=document.querySelector ("#description");
-    let temperatureElement = document.querySelector("#temperature");
-    let dateElement=document.querySelector("#date");
-    let iconElement=document.querySelector ("#icon");
-      temperatureElement.innerHTML = `${Math.round(response.data.main.temp)}°F|°C`;
-    dateElement.innerHTML= formatDate(response.data.dt*1000);
-    descriptionElement.innerHTML=(response.data.weather[0].description);
-    humidityElement.innerHTML= `${(response.data.main.humidity)}%`;
-    iconElement.setAttribute ("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
   }
-  function search(city) {
-    let apiKey = "3178cd4dc212e1aed68087c7e06933d5";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
-    axios.get(apiUrl).then(showTemperature);
-  }
-  function handleSubmit (event){
-    event.preventDefault();
-    let searchTextInputElement = document.querySelector("#search-text-input");
-    search(searchTextInputElement.value);
-  }
- 
-  
-  let units = "imperial";
-  let searchForm = document.querySelector("#search-form");
-  searchForm.addEventListener("submit", handleSubmit);
+  let dayIndex = date.getDay();
+  let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+  let day = days[dayIndex];
+  return `${day} ${hours}:${minutes}`;
+}
+function showTemperature(response) {
+  console.log(response.data.main.temp);
+  let humidityElement = document.querySelector("#humidity");
+  let descriptionElement = document.querySelector("#description");
+  let temperatureElement = document.querySelector("#temperature");
+  let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+  let wind = Math.round(response.data.wind.speed);
+  let currentWind = document.querySelector("#wind");
+  currentWind.innerHTML = `${wind}mph`;
+  temperatureElement.innerHTML = `${Math.round(response.data.main.temp)}°F|°C`;
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = `${response.data.main.humidity}%`;
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+}
+function search(city) {
+  let apiKey = "3178cd4dc212e1aed68087c7e06933d5";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(showTemperature);
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  let searchTextInputElement = document.querySelector("#search-text-input");
+  search(searchTextInputElement.value);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchTextInputElement.value;
+}
+
+let units = "imperial";
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSubmit);
 
   
